8082bbbd066ab271c0c9b8331a14995e9bc3f102
Joshua Randall <jcrandall@alum.mit.edu>
2013-01-16 12:26:32 +0000

    Now automagically setting all Config.pm config options in database config table when set-db-config.sh is called.
    Changed run-api-command.pl to no longer use PB_PATH and pblib.cfg (only needed for CGI) -- just set PERL5LIB and PB_DEV_VERSION
    
    Fixed export_to_bash and export_to_tcsh to no longer output non-scalars (e.g. BEGIN, import, etc)

--- admin-tools/run-api-command.pl
+++ admin-tools/run-api-command.pl
@@ -1,11 +1,8 @@
 #!/usr/bin/env perl
 
-BEGIN {
-    use lib $ENV{'PB_PATH'}."/bin";
-    require 'pblib.cfg';
-}
-
 use PB::Config;
 use PB::API;
 
-eval(shift());
+my $evalstr = join(" ",@ARGV);
+print STDERR "evaling $evalstr\n";
+eval($evalstr);
