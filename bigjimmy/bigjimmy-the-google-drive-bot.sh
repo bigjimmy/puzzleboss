492f808b52dc9273297501ff024e77e3c40ee3e2
Benjamin OConnor <benoc@alum.mit.edu>
2014-12-24 19:49:35 +0000

    actually set GOPATH properly

--- bigjimmy/bigjimmy-the-google-drive-bot.sh
+++ bigjimmy/bigjimmy-the-google-drive-bot.sh
@@ -2,7 +2,9 @@
 
 eval $(perl -MPB::Config -e 'PB::Config::export_to_bash();')
 
-GOPATH=${PB_BIGJIMMY_PATH}
+export GOPATH=${PB_BIGJIMMY_PATH}
+
+echo PB_BIGJIMMY_PATH ${PB_BIGJIMMY_PATH}
 
 (cd ${PB_BIGJIMMY_PATH} && go install -gcflags "-N -l" bigjimmybot/bigjimmy-the-google-drive-bot && ./bin/bigjimmy-the-google-drive-bot $@)
 
