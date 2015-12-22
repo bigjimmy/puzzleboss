2465d2dbe12c72b7f26e71f2f78b5b56088ab5b3
Benjamin OConnor <benoc@alum.mit.edu>
2015-12-22 19:52:36 +0000

    generic slackcat.py script to put notifications in slack

---
+++ newgoogle/slackcat.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+import argparse
+import json
+import os
+import sys 
+
+import requests # pip install requests if you don't have it
+
+
+WEBHOOK_URL = 'https://hooks.slack.com/services/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'
+
+def post_to_slack(url, text, channel, user, emoji, quiet):
+    headers = {'content-type':'application/json'}
+    payload = json.dumps({
+            'channel' : channel,
+            'username' : user,
+            'text' : text,
+            'icon_emoji' : emoji, 
+    })
+
+    # if you don't want the dependency on the requests library, you can use the
+    # standard library here:
+    # import urllib2
+    #request = urllib2.Request(url, payload, headers) 
+    #f = urllib2.urlopen(request)
+    #response = f.read()
+    #f.close() 
+    # print response
+
+    # but requests is much nicer, particulary if there's an error
+    request = requests.post(url, headers=headers, data=payload)
+    if request.status_code == 200 and quiet :
+        # suppress output on successful post
+        pass
+    else :
+        print "Response: %s - %s" % (request.status_code, request.reason)
+
+
+def parse_args():
+    # try getting Webhook url from env variable
+    webhook_url = WEBHOOK_URL
+
+    parser = argparse.ArgumentParser(description='Talk to Slack')
+    parser.add_argument('-u','--url', 
+        help='Slack Incoming Webhooks integration webhook URL.', required=webhook_url is None, 
+        default=webhook_url) 
+    parser.add_argument('-c','--channel', help='Channel to post to', required=True)
+    parser.add_argument('-n','--user', help='Name of the user to post as',
+        required=True)
+    parser.add_argument('-e','--emoji', help='Emoji to use for the message', 
+        required=False, default=':rocket:')
+    parser.add_argument('-q','--quiet', help='Work quietly if post successful',
+        action='store_true')
+    parser.add_argument('-t','--text', help='Message to say. (uses stdin if not present)', required=False)
+
+    args = vars(parser.parse_args())
+
+    # ensure emoji has starting/ending colon
+    if not args['emoji'].startswith(':'):
+        args['emoji'] = ":" + args['emoji'] 
+    if not args['emoji'].endswith(':'):
+        args['emoji'] = args['emoji'] + ":" 
+
+    # ensure channel has pound sign
+    if args['channel'][0] not in ['#', '@']:
+        args['channel'] = '#' + args['channel']
+
+    return args
+
+
+def main():
+    args = parse_args()
+    text = args['text'] or sys.stdin.read()
+    post_to_slack(WEBHOOK_URL, text, args['channel'], args['user'], args['emoji'], args['quiet'])
+
+
+if __name__ == "__main__":
+    main()
