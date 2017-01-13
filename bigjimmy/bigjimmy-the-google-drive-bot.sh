a0db80c1074738c2f6fbf99ad46332f4b96487cc
Joshua Randall <jcrandall@alum.mit.edu>
2017-01-13 02:20:15 +0000

    simmer down to info level logging on bigjimmy

--- bigjimmy/bigjimmy-the-google-drive-bot.sh
+++ bigjimmy/bigjimmy-the-google-drive-bot.sh
@@ -4,4 +4,4 @@ eval $(perl -MPB::Config -e 'PB::Config::export_to_bash();')
 
 export GOPATH=${PB_BIGJIMMY_PATH}
 
-(cd ${PB_BIGJIMMY_PATH} && go install -gcflags "-N -l" bigjimmybot/bigjimmy-the-google-drive-bot && ./bin/bigjimmy-the-google-drive-bot --dbhost=localhost --dbuser=bigjimmy --dbpassword=fhqwhgads --dbname=puzzlebitch -log_level=trace)
+(cd ${PB_BIGJIMMY_PATH} && go install -gcflags "-N -l" bigjimmybot/bigjimmy-the-google-drive-bot && ./bin/bigjimmy-the-google-drive-bot --dbhost=localhost --dbuser=bigjimmy --dbpassword=fhqwhgads --dbname=puzzlebitch -log_level=info)
