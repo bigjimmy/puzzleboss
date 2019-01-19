43bd277ecc0db2b27421daddfd15a7a45c2296b4
Benjamin OConnor <benoc@alum.mit.edu>
2019-01-19 04:46:14 +0000

    make init-db restart apache/meteord fully

--- admin-tools/init-db.sh
+++ admin-tools/init-db.sh
@@ -13,12 +13,26 @@ if [[ $PHRASE != "IWANTTODESTROYTHEHUNT" ]]; then
 	else echo "OK. You asked for it."
 fi
 
+#STOP everything!
+/etc/init.d/apache2 stop
 /etc/init.d/bigjimmy stop
+
 
 eval $(perl -MPB::Config -e 'PB::Config::export_to_bash();')
 
 cat $PB_PATH/db/puzzleboss.create.sql-template | perl -pi -e 's/\$PB_DATA_DB_NAME/'$PB_DATA_DB_NAME'/g' | mysql -h $PB_DATA_DB_HOST -P $PB_DATA_DB_PORT -u $PB_DATA_DB_USER -p$PB_DATA_DB_PASS
 
+
+#Initialize config (in database) from Config.pm
 $PB_ADMIN_TOOLS_PATH/set-db-config.sh
 
+#Reset meteor log index and restart
+$PB_ADMIN_TOOLS_PATH/reset-log-index.sh
+/etc/init.d/meteord stop
+/etc/init.d/meteord start
+
+#Start up bigjimmy the google drive monitor
 /etc/init.d/bigjimmy start
+
+#restart apache for good measure
+/etc/init.d/apache2 start
