4abdc3646e5d9685f4bc9c74930a309698acf8df
Benjamin O'Connor <benoc@alum.mit.edu>
2020-01-04 20:33:02 +0000

    remove definers from sql stored procs

--- admin-tools/create-db-template.sh
+++ admin-tools/create-db-template.sh
@@ -3,7 +3,7 @@
 export PERL5LIB='.:/canadia/puzzleboss/lib'
 
 eval $(perl -MPB::Config -e 'PB::Config::export_to_bash();')
-mysqldump --add-drop-database -d --databases $PB_DATA_DB_NAME -u $PB_DATA_DB_USER -P $PB_DATA_DB_PORT -h $PB_DATA_DB_HOST --password=$PB_DATA_DB_PASS > $PB_PATH/db/puzzlebossnew
+mysqldump --add-drop-database --skip-definer -d --databases $PB_DATA_DB_NAME -u $PB_DATA_DB_USER -P $PB_DATA_DB_PORT -h $PB_DATA_DB_HOST --password=$PB_DATA_DB_PASS > $PB_PATH/db/puzzlebossnew
 cat $PB_PATH/db/puzzlebossnew | sed s/AUTO_INCREMENT=[[:digit:]]*//g | sed s/$PB_DATA_DB_NAME/\$PB_DATA_DB_NAME/g > $PB_PATH/db/puzzleboss.create.sql-template
 rm -rf $PB_PATH/db/puzzlebossnew
 
