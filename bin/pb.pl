1d3a2e653dcccbff255fea356eb74d27de835f1a
Benjamin O'Connor <benoc@alum.mit.edu>
2020-01-17 17:00:58 +0000

    add refresh to pages just in case

--- bin/pb.pl
+++ bin/pb.pl
@@ -45,6 +45,7 @@ Content-type: text/html
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta http-equiv="refresh" content="500"/>
 <title>$title</title>
     <style type="text/css">
         \@import "$PB::Config::DOJO_ROOT/dijit/themes/tundra/tundra.css";
