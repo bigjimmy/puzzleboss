066dd988e4b1b57e951fbfea8a575c7632969236
Benjamin O'Connor <benoc@alum.mit.edu>
2021-01-11 19:34:56 +0000

    preserve discord_users table in init-db

--- admin-tools/init-db.sh
+++ admin-tools/init-db.sh
@@ -23,9 +23,18 @@ ssh -t series-of-tubes-internal.wind-up-birds.org "sudo /etc/init.d/bigjimmy sto
 #Load current config
 eval $(perl -MPB::Config -e 'PB::Config::export_to_bash();')
 
+#Dump tables to preserve
+
+echo "Dumping solver and discord_users tables to $PB_PATH/backup/sometables.$$.sql"
+mysqldump -h $PB_DATA_DB_HOST -u $PB_DATA_DB_USER -P $PB_DATA_DB_PORT -p$PB_DATA_DB_PASS --add-drop-table puzzleboss discord_users > $PB_PATH/backup/sometables.$$.sql
+
 #Drop and Re-create database using the sql template
+echo "Destroying database and reloading saved schema"
 cat $PB_PATH/db/puzzleboss.create.sql-template | perl -pi -e 's/\$PB_DATA_DB_NAME/'$PB_DATA_DB_NAME'/g' | mysql -h $PB_DATA_DB_HOST -P $PB_DATA_DB_PORT -u $PB_DATA_DB_USER -p$PB_DATA_DB_PASS
 
+#Restore previously preserved tables
+echo "Restoring solver and discord_users tables"
+cat $PB_PATH/backup/sometables.$$.sql | mysql -h $PB_DATA_DB_HOST -P $PB_DATA_DB_PORT -u $PB_DATA_DB_USER -p$PB_DATA_DB_PASS puzzleboss
 
 #Initialize config (in database) 
 $PB_ADMIN_TOOLS_PATH/set-db-config.sh
