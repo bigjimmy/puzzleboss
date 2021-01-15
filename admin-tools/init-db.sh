9665a8af7b886e8e1616b26390dc40f137af749f
Benjamin O'Connor <benoc@alum.mit.edu>
2021-01-15 02:45:53 +0000

    Revert "further bulletproof db-init and save full dump"
    This reverts commit 2fd90ca3873bc8015cbdc33c32b5d81e83e89f11.

--- admin-tools/init-db.sh
+++ admin-tools/init-db.sh
@@ -1,18 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash
 
 export PERL5LIB='.:/canadia/puzzleboss/lib'
-
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
 
 echo "==WARNING!!!===WARNING!!!===WARNING!!!===WARNING=="
 echo "Running this command will ERASE ALL PROGRESS AND DATA FROM PB."
 echo "DO NOT DO THIS DURING HUNT!"
-echo ""
-echo "Please verify that you are running this as ROOT from the backend server (series-of-tubes)"
-echo ""
+
 echo "Enter the phrase IWANTTODESTROYTHEHUNT to continue:"
 read PHRASE
 
@@ -23,49 +16,39 @@ if [[ $PHRASE != "IWANTTODESTROYTHEHUNT" ]]; then
 fi
 
 #STOP everything!
-
-echo "Stopping s-o-t apache"
-/etc/init.d/apache2 stop
-
-echo "Stopping bigjimmy bot"
-/etc/init.d/bigjimmy stop
+sudo /etc/init.d/apache2 stop
+ssh -t series-of-tubes-internal.wind-up-birds.org "sudo /etc/init.d/bigjimmy stop"
 
 
 #Load current config
 eval $(perl -MPB::Config -e 'PB::Config::export_to_bash();')
 
 #Dump tables to preserve
-echo "Dumping discord_users table to $PB_PATH/backup/sometables.$$.sql"
-mysqldump -h $PB_DATA_DB_HOST -u $PB_DATA_DB_USER -P $PB_DATA_DB_PORT -p$PB_DATA_DB_PASS --add-drop-table puzzleboss discord_users > $PB_PATH/backup/sometables.$$.sql
 
-echo "Dumping entire mysql database to $PB_PATH/backup/mysqldump-YYYY-MM-DD-HHMMSS.sql"
-mysqldump -h $PB_DATA_DB_HOST -u $PB_DATA_DB_USER -P $PB_DATA_DB_PORT -p$PB_DATA_DB_PASS --add-drop-table puzzleboss > $PB_PATH/backup/mysqldump-`date +%Y-%m-%d-%H%M%S`.sql
+echo "Dumping solver and discord_users tables to $PB_PATH/backup/sometables.$$.sql"
+mysqldump -h $PB_DATA_DB_HOST -u $PB_DATA_DB_USER -P $PB_DATA_DB_PORT -p$PB_DATA_DB_PASS --add-drop-table puzzleboss discord_users > $PB_PATH/backup/sometables.$$.sql
 
 #Drop and Re-create database using the sql template
 echo "Destroying database and reloading saved schema"
 cat $PB_PATH/db/puzzleboss.create.sql-template | perl -pi -e 's/\$PB_DATA_DB_NAME/'$PB_DATA_DB_NAME'/g' | mysql -h $PB_DATA_DB_HOST -P $PB_DATA_DB_PORT -u $PB_DATA_DB_USER -p$PB_DATA_DB_PASS
 
 #Restore previously preserved tables
-echo "Restoring discord_users table"
+echo "Restoring solver and discord_users tables"
 cat $PB_PATH/backup/sometables.$$.sql | mysql -h $PB_DATA_DB_HOST -P $PB_DATA_DB_PORT -u $PB_DATA_DB_USER -p$PB_DATA_DB_PASS puzzleboss
 
-echo "Re-loading config into database"
 #Initialize config (in database) 
 $PB_ADMIN_TOOLS_PATH/set-db-config.sh
 
-echo "Resetting meteor log index and restarting meteord"
 #Reset meteor log index and restart
 $PB_ADMIN_TOOLS_PATH/reset-log-index.sh
-pkill -f meteord
-/etc/init.d/meteord start
+ssh -t series-of-tubes-internal.wind-up-birds.org "sudo /etc/init.d/meteord stop"
+ssh -t series-of-tubes-internal.wind-up-birds.org "sudo /etc/init.d/meteord start"
 
-echo "Restarting bigjimmy bot"
 #Start up bigjimmy the google drive monitor
-/etc/init.d/bigjimmy start
+ssh -t series-of-tubes-internal.wind-up-birds.org "sudo /etc/init.d/bigjimmy start"
 
-echo "Restarting s-o-t apache"
 #restart apache for good measure
-/etc/init.d/apache2 start
+sudo /etc/init.d/apache2 start
 
 #Purge puzzcord discord stuff
 echo "Purging discord stuff"
